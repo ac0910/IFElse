0131e6fcca41a19b9ee8c26a8b0dfad9940b9b14
ac0910 <118996679+ac0910@users.noreply.github.com>
2023-09-11 17:06:56 +0530

    Add files via upload

---
+++ oddeven.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int n;
+    printf("Entaer a Number :");
+    scanf("%d",&n);
+    if (n%2==0)
+    {
+        printf("Even Number");
+    
+    }
+    else{
+        printf("Odd Number");
+    }
+    
+    return 0;
+}
